2f35e57879dc320894d054bdff927933ddea6db0
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-11 18:56:56 +0300

    Save progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('signup', views.signup, name='signup')
+    path('signup/', views.signup, name='signup')
 ]
