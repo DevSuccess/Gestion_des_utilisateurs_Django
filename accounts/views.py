4d79c1eb5771fe383452cdfb32b768b2983d6507
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-11 11:30:21 +0300

    Ajout de view. SAVE :  https://www.tresfacile.net/gestion-des-utilisateurs-django/

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('/')
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'registration/signup.html'), context
